b7d00eefba99fa446596afe1abc3ac92a204f349
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-04-28 20:05:04 +0300

    create SetUp()

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,7 +1,27 @@
 package edu.iis.mto.time;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.mockito.Mock;
 
+import java.time.Clock;
+import java.time.Instant;
+
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 public class OrderTest {
+    @Mock
+    private Clock clock;
+    private Instant currentTime;
+    private Order order;
+
+    @Before
+    public void setUp() {
+        currentTime = Instant.now();
+        clock = mock(Clock.class);
+        order = new Order(clock);
+        when(clock.instant()).thenAnswer((invocation) -> currentTime);
+    }
 
 }
