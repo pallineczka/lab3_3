4de25daabe5fc43f4162f555e98a594c8f5cf959
209420 <209420@edu.p.lodz.pl>
2019-05-12 19:04:41 +0200

    make orderTest, create init method and test call_orderconfirm_should_throw_OrderExpiredException

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,40 @@
+package edu.iis.mto.time;
+
+
+import org.junit.jupiter.api.*;
+
+import org.mockito.Mockito;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+public class OrderTest {
+
+    private Clock clock;
+    private Instant time;
+    private Order order;
+
+
+    @BeforeEach
+    public void init() {
+        time = Instant.now();
+        clock = mock(Clock.class);
+        order = new Order(clock);
+        when(clock.instant()).thenAnswer((invocation) -> time);
+    }
+
+    @Test
+    public void call_orderconfirm_should_throw_OrderExpiredException() {
+        order.submit();
+        time = time.plus(25, ChronoUnit.HOURS);
+        Mockito.when(clock.instant()).thenReturn(time);
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+    }
+
+
+
+}
